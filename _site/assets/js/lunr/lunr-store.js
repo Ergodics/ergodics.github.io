245597e4298e4a5e6dd7d887486b1c954802e270
ergodics <ergodics@proton.me>
2024-05-26 01:08:19 -0400

    Updated config

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Hello World",
         "excerpt":"Hello, World   Reveries in Root, adventures in cybersecurity by Ergodics   def hello_world():   print(\"Hello, World\")  ","categories": [],
         "tags": [],
-        "url": "/2024/05/26/hello-world.html",
+        "url": "/hello-world/",
         "teaser": null
       }]
